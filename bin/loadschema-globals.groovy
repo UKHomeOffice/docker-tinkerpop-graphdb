6f6d1beade4d6b1b58eebb28df67e42a6bc63779
Pontus <lmartins@pontusnetworks.com>
2019-04-09 15:23:22 +0100

    fixed globals.groovy issues

--- bin/loadschema-globals.groovy
+++ bin/loadschema-globals.groovy
@@ -1,6 +1,17 @@
+import com.pontusvision.utils.ElasticSearchHelper
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import groovy.transform.CompileDynamic
+import groovy.transform.CompileStatic
+import org.apache.commons.math3.util.Pair
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
-import org.janusgraph.core.JanusGraph
-import org.janusgraph.core.schema.JanusGraphManagement
+import org.apache.tinkerpop.gremlin.structure.Edge
+import org.apache.tinkerpop.gremlin.structure.Vertex
+import org.janusgraph.core.*
+import org.janusgraph.core.schema.*
+import org.janusgraph.graphdb.types.vertices.JanusGraphSchemaVertex
+
+import java.util.concurrent.atomic.AtomicInteger
 
 LinkedHashMap globals = [:]
 globals << [g: ((JanusGraph) graph).traversal() as GraphTraversalSource]
